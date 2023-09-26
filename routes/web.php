f766c28740e1ebb97c4d5de48039d6850f6e01f9
AkmalYonan <akmalyonanda@gmail.com>
2023-09-26 22:00:39 +0700

    Add Middleware

--- routes/web.php
+++ routes/web.php
@@ -26,28 +26,24 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 //admin
-Route::middleware('auth')->group(function () {
-
+Route::middleware('auth', 'admin')->group(function () {
     Route::get('/admin/meja', [AdminController::class, 'indexMeja'])->name('admin.meja');
     Route::post('/admin/tambahmeja', [AdminController::class, 'storeMeja'])->name('admin.tambahMeja');
     //Route::get('/admin/ubahmeja/{id}', [AdminController::class, 'ubahMeja'])->name('admin.ubahMeja');
     //Route::put('/admin/editmeja/{id}', [AdminController::class, 'editMeja'])->name('admin.editMeja');
     Route::post('/admin/hapusmeja', [AdminController::class, 'hapusMeja'])->name('admin.hapusMeja');
-
-
     Route::get('/admin/menu', [AdminController::class, 'indexMenu'])->name('admin.menu');
     Route::post('/admin/tambahMenu', [AdminController::class, 'storeMenu'])->name('admin.tambahMenu');
     Route::get('/admin/ubahmenu/{id}', [AdminController::class, 'ubahMenu'])->name('admin.ubahMenu');
     Route::put('/admin/editmenu/{id}', [AdminController::class, 'editMenu'])->name('admin.editMenu');
     Route::delete('/admin/hapusmenu/{id}', [AdminController::class, 'hapusMenu'])->name('admin.hapusMenu');
 
-
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::middleware('auth')->group(function () {
+
+Route::middleware('auth', 'waiter')->group(function () {
     Route::get('/waiter', [WaiterController::class, 'index'])->name('waiter.index');
     Route::get('/waiter/cart', [WaiterController::class, 'showCart'])->name('waiter.cart');
     Route::get('/waiter/addcart/{id}', [WaiterController::class, 'addCart'])->name('addorder.cart');
